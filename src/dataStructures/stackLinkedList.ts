cd6bd4583b7624c00f422c7dc50438ff7115fe5d
Alisson Vargas <alisson.vargass@hotmail.com>
2021-05-09 23:56:59 -0300

    feat: add stackLinkedList

---
+++ src/dataStructures/stackLinkedList.ts
@@ -0,0 +1,40 @@
+import DoublyLinkedList from './doublyLinkedList';
+
+export default class StackLinkedList<T> {
+  private items: DoublyLinkedList<T>;
+
+  constructor() {
+    this.items = new DoublyLinkedList();
+  }
+
+  push(element: T): void {
+    return this.items.push(element);
+  }
+
+  pop(): T {
+    if (this.items.isEmpty()) {
+      return undefined;
+    }
+    return this.items.removeAt(this.items.size() - 1);
+  }
+
+  size(): number {
+    return this.items.size();
+  }
+
+  isEmpty(): boolean {
+    return this.items.isEmpty();
+  }
+
+  peek(): T {
+    return this.items.getTail().element;
+  }
+
+  clear(): void {
+    this.items.clear();
+  }
+
+  toString(): string {
+    return this.items.toString();
+  }
+}
---
+++ tests/dataStructures/stackLinkedList.test.ts
@@ -0,0 +1,69 @@
+import StackLinkedList from '../../src/dataStructures/stackLinkedList';
+
+describe('Stack Linked List', () => {
+  let list: StackLinkedList<number>;
+  
+  function insertElements() {
+    list.push(1);
+    list.push(2);
+    list.push(3);
+  }
+
+  beforeEach(() => {
+    list = new StackLinkedList<number>();
+  });
+
+  it('add elements to the list', () => {
+    list.push(1);
+    expect(list.size()).toBe(1);
+    list.push(2);
+    expect(list.size()).toBe(2);
+    list.push(3);
+    expect(list.size()).toBe(3);
+  });
+
+  describe('Pop function', () => {
+    it('pop the last element from the list', () => {
+      insertElements();
+      expect(list.size()).toBe(3);
+      
+      expect(list.pop()).toBe(3);
+      expect(list.size()).toBe(2);
+    });
+
+    it('return undefined if the list is empty', () => {
+      expect(list.pop()).toBeUndefined()
+    });
+  });
+
+
+  it('returns the size of the list', () => {
+    expect(list.size()).toBe(0);
+    insertElements();
+    expect(list.size()).toBe(3);
+  });
+
+  it('return true if the list is empty', () => {
+    expect(list.isEmpty()).toBeTruthy();
+    insertElements();
+    expect(list.isEmpty()).toBeFalsy();
+  });
+
+  it('peek the last element of the list', () => {
+    insertElements();
+    expect(list.peek()).toBe(3);
+  });
+
+  it('clear the list', () => {
+    insertElements();
+    expect(list.size()).toBe(3);
+
+    list.clear();
+    expect(list.size()).toBe(0);
+  });
+
+  it('return the list as string', () => {
+    insertElements();
+    expect(list.toString()).toBe('1, 2, 3');
+  });
+});

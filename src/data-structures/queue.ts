c6f8ba5e2d34be8406b89d61e2a2eb860e7d7ee7
Alisson Vargas <contato@alissonsv.com>
2025-05-04 18:01:01 -0300

    refactor: add lowestKey to queue

--- src/data-structures/queue.ts
+++ src/data-structures/queue.ts
@@ -1,16 +1,19 @@
 export default class Queue<T> {
-  private count: number;
+  private highestKey: number;
+
+  private lowestKey: number;
 
   private items: object;
 
   constructor() {
-    this.count = 0;
+    this.highestKey = 0;
+    this.lowestKey = 0;
     this.items = {};
   }
 
   enqueue(element: T) {
-    this.items[this.count] = element;
-    this.count++;
+    this.items[this.highestKey] = element;
+    this.highestKey++;
   }
 
   dequeue() {
@@ -18,11 +21,9 @@ export default class Queue<T> {
       return undefined;
     }
 
-    const result = this.items[0];
-    for (let i = 0; i < this.count; i++) {
-      this.items[i] = this.items[i + 1];
-    }
-    this.count--;
+    const result = this.items[this.lowestKey];
+    delete this.items[this.lowestKey]
+    this.lowestKey++;
 
     return result;
   }
@@ -31,28 +32,29 @@ export default class Queue<T> {
     if (this.isEmpty()) {
       return undefined;
     }
-    return this.items[0];
+    return this.items[this.lowestKey];
   }
 
   isEmpty() {
-    return this.count === 0;
+    return this.size() === 0;
   }
 
   size() {
-    return this.count;
+    return this.highestKey - this.lowestKey;
   }
 
   clear() {
     this.items = {};
-    this.count = 0;
+    this.highestKey = 0;
+    this.lowestKey = 0;
   }
 
   toString() {
     if (this.isEmpty()) {
       return '';
     }
-    let objString = `${this.items[0]}`;
-    for (let i = 1; i < this.count; i++) {
+    let objString = `${this.items[this.lowestKey]}`;
+    for (let i = this.lowestKey + 1; i < this.highestKey; i++) {
       objString = `${objString}, ${this.items[i]}`;
     }
 

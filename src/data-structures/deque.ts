39a25d683153eb3f0100640aa465049df775fbe1
Alisson Vargas <contato@alissonsv.com>
2025-05-04 18:38:28 -0300

    refactor: add lowestKey to deque

--- src/data-structures/deque.ts
+++ src/data-structures/deque.ts
@@ -1,68 +1,75 @@
 export default class Deque<T> {
-  private count: number;
+  private highestKey: number;
+
+  private lowestKey: number;
 
   private items: object;
 
   constructor() {
-    this.count = 0;
+    this.highestKey = 0;
+    this.lowestKey = 0;
     this.items = {};
   }
 
   addFront(element: T) {
     if (this.isEmpty()) {
       this.addBack(element);
+    } else if (this.lowestKey > 0) {
+      this.lowestKey--;
+      this.items[this.lowestKey] = element
     } else {
-      for (let i = this.count; i > 0; i--) {
+      for (let i = this.highestKey; i > 0; i--) {
         this.items[i] = this.items[i - 1];
       }
+      this.highestKey++;
+      this.lowestKey = 0;
       this.items[0] = element;
-      this.count++;
     }
   }
 
   addBack(element: T) {
-    this.items[this.count] = element;
-    this.count++;
+    this.items[this.highestKey] = element;
+    this.highestKey++;
   }
 
   removeFront():T {
-    const result = this.items[0];
-    for (let i = 0; i < this.count; i++) {
-      this.items[i] = this.items[i + 1];
-    }
+    const result = this.items[this.lowestKey];
+    delete this.items[this.lowestKey]
 
-    this.count--;
+    this.lowestKey++;
+
     return result;
   }
 
   removeBack():T {
-    const result = this.items[this.count - 1];
+    const result = this.items[this.highestKey - 1];
 
-    this.items[this.count - 1] = undefined;
-    this.count--;
+    this.items[this.highestKey - 1] = undefined;
+    this.highestKey--;
 
     return result;
   }
 
   peekFront():T {
-    return this.items[0];
+    return this.items[this.lowestKey];
   }
 
   peekBack():T {
-    return this.items[this.count - 1];
+    return this.items[this.highestKey - 1];
   }
 
-  isEmpty():boolean {
-    return this.count === 0;
+  isEmpty(): boolean {
+    return this.size() === 0;
   }
 
   clear() {
     this.items = {};
-    this.count = 0;
+    this.lowestKey = 0;
+    this.highestKey = 0;
   }
 
-  size():number {
-    return this.count;
+  size(): number {
+    return this.highestKey - this.lowestKey;
   }
 
   toString():string {
@@ -71,7 +78,7 @@ export default class Deque<T> {
     }
 
     let objString = this.items[0];
-    for (let i = 1; i < this.count; i++) {
+    for (let i = 1; i < this.highestKey; i++) {
       objString = `${objString}, ${this.items[i]}`;
     }
 
